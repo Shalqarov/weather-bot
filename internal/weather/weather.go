8ecef01999cac0795524fa786969a7323734e282
shalqarov <shalqarov01@gmail.com>
2023-03-17 22:39:07 +0600

    update: weather function

--- internal/weather/weather.go
+++ internal/weather/weather.go
@@ -4,20 +4,19 @@ import (
 	"fmt"
 	"math"
 
+	"github.com/Shalqarov/weather-bot/config"
 	owm "github.com/briandowns/openweathermap"
 )
 
-func CurrentTemperature(location string, apiKey string) (string, error) {
-	w, err := owm.NewCurrent("C", "ru", apiKey)
+func CurrentTemperature(location string) (string, error) {
+	w, err := owm.NewCurrent("C", "ru", config.GetConfig().WeatherAPI)
 	if err != nil {
 		return "", err
 	}
-
-	err = w.CurrentByName("Astana")
+	err = w.CurrentByName(location)
 	if err != nil {
 		return "", err
 	}
-
 	return fmt.Sprintf(
 		"Локация: %s\nТемпература воздуха: %.f°C",
 		location, math.Round(w.Main.Temp),
