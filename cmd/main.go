8cff88363b9bc442ff9f6790e7f1d4cb94516ec1
shalqarov <shalqarov01@gmail.com>
2023-03-18 20:33:06 +0600

    initialization refactoring

--- cmd/main.go
+++ cmd/main.go
@@ -1,26 +1,15 @@
 package main
 
 import (
-	"github.com/Shalqarov/weather-bot/config"
 	initialize "github.com/Shalqarov/weather-bot/init"
 	"github.com/Shalqarov/weather-bot/internal/app"
-	"github.com/Shalqarov/weather-bot/internal/bot"
-	"github.com/Shalqarov/weather-bot/internal/database/postgres"
-	"github.com/Shalqarov/weather-bot/pkg"
 )
 
-const configPath = "./config.toml"
-
 func init() {
-	// Config init
-	config.GetConfig()
-	initialize.Migration("./internal/database/postgres/migrations/")
-	// db init
-	postgres.GetDB()
-	// users init
-	pkg.UsersMap()
-	// bot init
-	bot.GetBot()
+	initialize.Config()
+	initialize.Migration()
+	initialize.DB()
+	initialize.Bot()
 }
 
 func main() {
--- init/init.go
+++ init/init.go
@@ -5,11 +5,29 @@ import (
 	"os"
 	"sort"
 
+	"github.com/Shalqarov/weather-bot/config"
+	"github.com/Shalqarov/weather-bot/internal/bot"
 	"github.com/Shalqarov/weather-bot/internal/database/postgres"
 )
 
-func Migration(dir string) {
-	f, err := os.Open(dir)
+const (
+	migrationPath = "./internal/database/postgres/migrations/"
+)
+
+func Config() {
+	config.GetConfig()
+}
+
+func DB() {
+	postgres.GetDB()
+}
+
+func Bot() {
+	bot.GetBot()
+}
+
+func Migration() {
+	f, err := os.Open(migrationPath)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -24,7 +42,7 @@ func Migration(dir string) {
 
 	db := postgres.GetDB()
 	for _, file := range files {
-		path := dir + file
+		path := migrationPath + file
 		query, err := os.ReadFile(path)
 		if err != nil {
 			log.Fatalln(err)
