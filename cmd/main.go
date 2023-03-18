610a2891d3e2441c601ce831e9a883d260a176b6
shalqarov <shalqarov01@gmail.com>
2023-03-18 18:53:17 +0600

    add: migration ini

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"github.com/Shalqarov/weather-bot/config"
+	initialize "github.com/Shalqarov/weather-bot/init"
 	"github.com/Shalqarov/weather-bot/internal/app"
 	"github.com/Shalqarov/weather-bot/internal/bot"
+	"github.com/Shalqarov/weather-bot/internal/database/postgres"
 	"github.com/Shalqarov/weather-bot/pkg"
 )
 
@@ -12,6 +14,9 @@ const configPath = "./config.toml"
 func init() {
 	// Config init
 	config.GetConfig()
+	initialize.Migration("./internal/database/postgres/migrations/")
+	// db init
+	postgres.GetDB()
 	// users init
 	pkg.UsersMap()
 	// bot init
