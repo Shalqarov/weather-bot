b90cfccb92180b44e59db9511f5d3f4b20e52760
shalqarov <shalqarov01@gmail.com>
2023-03-18 14:56:43 +0600

    add: bot and config init

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,19 @@ package main
 import (
 	"github.com/Shalqarov/weather-bot/config"
 	"github.com/Shalqarov/weather-bot/internal/app"
+	"github.com/Shalqarov/weather-bot/internal/bot"
+	"github.com/Shalqarov/weather-bot/pkg"
 )
 
 const configPath = "./config.toml"
 
 func init() {
-	// Config initialization
+	// Config init
 	config.GetConfig()
+	// users init
+	pkg.UsersMap()
+	// bot init
+	bot.GetBot()
 }
 
 func main() {
--- internal/app/app.go
+++ internal/app/app.go
@@ -1,6 +1,9 @@
 package app
 
 import (
+	"log"
+
+	"github.com/Shalqarov/weather-bot/internal/bot"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
@@ -8,4 +11,9 @@ func Run() {
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
+	updates := bot.GetBot().GetUpdatesChan(u)
+	err := bot.HandleUpdates(updates)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
 }
